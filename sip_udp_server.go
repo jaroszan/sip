19ae7765c17ccad1086263966ddbbb2dd871902b
jaroszan <jaroslaw.szangin@gmail.com>
2015-06-23 15:08:13 +0200

    changed API for building response

--- sip_udp_server.go
+++ sip_udp_server.go
@@ -15,22 +15,38 @@ const (
 	RESPONSE = "response"
 )
 
-func makeResponse(sipHeaders map[string]string, code string, reasonPhrase string, method string) []byte {
-	var response bytes.Buffer
-	response.WriteString("SIP/2.0 " + code + " " + reasonPhrase + "\r\n")
-	response.WriteString("Via: " + sipHeaders["Via"] + "\r\n")
-	response.WriteString("To: " + sipHeaders["To"] + "\r\n")
-	response.WriteString("From: " + sipHeaders["From"] + "\r\n")
-	response.WriteString("Call-ID: " + sipHeaders["Call-ID"] + "\r\n")
-	response.WriteString("CSeq: " + sipHeaders["CSeq"] + "\r\n")
-	response.WriteString("Content-Length: 0\r\n")
-	if method != "BYE" && code != "100" {
-		response.WriteString("Contact: sip:bob@localhost:5060\r\n")
-	}
-	response.WriteString("Max-Forwards: " + sipHeaders["Max-Forwards"] + "\r\n" + "\r\n")
-	return response.Bytes()
+// Prepare a response to incoming request
+func prepareResponse(sipHeaders map[string]string, code string, reasonPhrase string) string {
+	var response string
+	response += "SIP/2.0 " + code + " " + reasonPhrase + "\r\n"
+	response += "Via: " + sipHeaders["Via"] + "\r\n"
+	response += "To: " + sipHeaders["To"] + "\r\n"
+	response += "From: " + sipHeaders["From"] + "\r\n"
+	response += "Call-ID: " + sipHeaders["Call-ID"] + "\r\n"
+	response += "CSeq: " + sipHeaders["CSeq"] + "\r\n"
+	response += "Max-Forwards: " + sipHeaders["Max-Forwards"] + "\r\n"
+	return response
 }
 
+// Adding header to response, should be called after prepareResponse to add non-mandatory headers
+func addHeader(responseHeaders string, newHeaderName string, newHeaderValue string) string {
+	responseHeaders += newHeaderName + ": " + newHeaderValue + "\r\n"
+	return responseHeaders
+}
+
+// Finalizing response and putting it on the wire
+func sendResponse(responseHeaders string, addr *net.UDPAddr, connection *net.UDPConn) {
+	var payload bytes.Buffer
+	responseHeaders += "\r\n"
+	payload.WriteString(responseHeaders)
+	_, err := connection.WriteToUDP(payload.Bytes(), addr)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(0)
+	}
+}
+
+// Parse first line to determine whether incoming message is a response or a request
 func parseFirstLine(ruriOrStatusLine string) (string, string, error) {
 	firstLine := strings.Fields(ruriOrStatusLine)
 	if firstLine[2] == "SIP/2.0" {
@@ -42,52 +58,46 @@ func parseFirstLine(ruriOrStatusLine string) (string, string, error) {
 	}
 }
 
-//func parseHeaders(
-
-func handleIncomingPacket(buf []byte, addr *net.UDPAddr, packetSize int, connection *net.UDPConn) {
+// Parsing headers, so far SDP is not processed in any way
+func parseHeaders(headers []string) map[string]string {
 	sipHeaders := make(map[string]string)
-	payload := string(buf[0:packetSize])
-	lines := strings.Split(payload, "\n")
-	mType, mValue, err := parseFirstLine(lines[0])
-	fmt.Println(mType, " ", mValue, " received")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	for _, value := range lines[1:] {
+	for _, value := range headers {
 		header := strings.SplitN(value, ":", 2)
-		// Ignoring body after headers are processed
 		if len(header) != 2 {
+			// TODO: processing of SDP body
 			break
 		}
 		sipHeaders[strings.TrimSpace(header[0])] = strings.TrimSpace(header[1])
 	}
+	return sipHeaders
+}
 
+func handleIncomingPacket(buf []byte, addr *net.UDPAddr, packetSize int, connection *net.UDPConn) {
+	payload := string(buf[0:packetSize])
+	lines := strings.Split(payload, "\n")
+	_, mValue, err := parseFirstLine(lines[0])
+	//fmt.Println(mType, " ", mValue, " received")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	sipHeaders := parseHeaders(lines[1:])
 	if mValue == "INVITE" {
-		outboundTrying := makeResponse(sipHeaders, "100", "Trying", mValue)
-		outboundOK := makeResponse(sipHeaders, "200", "", mValue)
-		_, err := connection.WriteToUDP(outboundTrying, addr)
-		if err != nil {
-			fmt.Println("Error: ", err)
-			os.Exit(0)
-		}
-		_, err = connection.WriteToUDP(outboundOK, addr)
-		if err != nil {
-			fmt.Println("Error: ", err)
-			os.Exit(0)
-		}
+		outboundTrying := prepareResponse(sipHeaders, "100", "Trying")
+		outboundOK := prepareResponse(sipHeaders, "200", "OK")
+		outboundOK = addHeader(outboundOK, "Contact", "sip:bob@localhost:5060")
+		sendResponse(outboundTrying, addr, connection)
+		sendResponse(outboundOK, addr, connection)
+
 	} else if mValue == "BYE" {
-		outboundOK := makeResponse(sipHeaders, "200", "OK", mValue)
-		_, err := connection.WriteToUDP(outboundOK, addr)
-		if err != nil {
-			fmt.Println("Error: ", err)
-			os.Exit(0)
-		}
+		outboundOK := prepareResponse(sipHeaders, "200", "OK")
+		sendResponse(outboundOK, addr, connection)
 	} else {
 	}
 }
 
 func listen(connection *net.UDPConn, quit chan struct{}) {
+	// 4 Kb seems sufficient for SIP packet
 	buffer := make([]byte, 4096)
 	n, remoteAddr, err := 0, new(net.UDPAddr), error(nil)
 	for err == nil {
@@ -110,7 +120,7 @@ func main() {
 	}
 	quit := make(chan struct{})
 	for i := 0; i < runtime.NumCPU(); i++ {
-		fmt.Println(runtime.NumCPU(), " processor(s) started")
+		fmt.Println(runtime.NumCPU(), " listener(s) started as determined by runtime.NumCPU()")
 		go listen(connection, quit)
 	}
 	<-quit
