b33a1f818f3e60c74cbc09002e8b08d6c4692f3b
jaroszan <jaroslaw.szangin@gmail.com>
2016-12-26 13:36:03 +0100

    changes in api, adding basic body processing

--- connection_tcp.go
+++ connection_tcp.go
@@ -9,12 +9,11 @@ import (
 	"bytes"
 	"log"
 	"net"
+	"strconv"
 )
 
 // StartTCPClient prepares TCP connection and returns inbound/outbound channels
-func StartTCPClient(lAddr string, rAddr string) (chan []byte, chan []byte, *net.TCPConn) {
-	//var wg sync.WaitGroup
-	//wg.Add(2)
+func StartTCPClient(lAddr string, rAddr string) (chan SipMessage, chan SipMessage, *net.TCPConn) {
 	//Resolve local address
 	localTcpAddr, err := net.ResolveTCPAddr("tcp4", lAddr)
 	CheckConnError(err)
@@ -28,9 +27,9 @@ func StartTCPClient(lAddr string, rAddr string) (chan []byte, chan []byte, *net.
 	CheckConnError(err)
 
 	// Outbound channel uses connection to send messages
-	outbound := make(chan []byte)
+	outbound := make(chan SipMessage)
 	// Inbound channel passes received message to handleIncomingPacket function
-	inbound := make(chan []byte)
+	inbound := make(chan SipMessage)
 
 	// Goroutine for receiving messages and passing them to handleIncomingPacket function
 	go recvTCP(conn, inbound)
@@ -41,9 +40,9 @@ func StartTCPClient(lAddr string, rAddr string) (chan []byte, chan []byte, *net.
 
 }
 
-func sendTCP(connection *net.TCPConn, outbound chan []byte) {
+func sendTCP(connection *net.TCPConn, outbound chan SipMessage) {
 	for message := range outbound {
-		_, err := connection.Write(message)
+		_, err := connection.Write(serializeSipMessage(message))
 		if err != nil {
 			log.Println("Error on write: ", err)
 			continue
@@ -51,25 +50,57 @@ func sendTCP(connection *net.TCPConn, outbound chan []byte) {
 	}
 }
 
-func recvTCP(connection *net.TCPConn, inbound chan []byte) {
+//TODO put mandatory headers first
+func serializeSipMessage(sipMessage SipMessage) []byte {
+	var serializedMessage bytes.Buffer
+	serializedMessage.WriteString(sipMessage.FirstLine)
+	serializedMessage.WriteString("\r\n")
+	for name, value := range sipMessage.Headers {
+		serializedMessage.WriteString(name)
+		serializedMessage.WriteString(": ")
+		serializedMessage.WriteString(value)
+		serializedMessage.WriteString("\r\n")
+	}
+	serializedMessage.WriteString("\r\n")
+	if len(sipMessage.Body) > 0 {
+		serializedMessage.WriteString(sipMessage.Body)
+		serializedMessage.WriteString("\r\n")
+	}
+	return serializedMessage.Bytes()
+}
+
+func recvTCP(connection *net.TCPConn, inbound chan SipMessage) {
+	reader := bufio.NewReader(connection)
 	for {
-		scanner := bufio.NewScanner(connection)
-		onSipDelimiter := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-			delim := []byte{'\r', '\n', '\r', '\n'}
-			if atEOF && len(data) == 0 {
-				return 0, nil, nil
+		var buffer bytes.Buffer
+		for {
+			chunk, err := reader.ReadString('\n')
+			if err != nil {
+				log.Println(err)
 			}
-			if i := bytes.Index(data, delim); i > 0 {
-				return i + len(delim), data[0:i], nil
+			buffer.WriteString(chunk)
+			if len(chunk) == 2 {
+				break
 			}
-			if atEOF {
-				return len(data), data, nil
-			}
-			return 0, nil, nil
 		}
-		scanner.Split(onSipDelimiter)
-		for scanner.Scan() {
-			inbound <- scanner.Bytes()
+		firstLine, sipHeaders, _ := ParseIncomingMessage(buffer.Bytes())
+		contentLength, err := strconv.Atoi(sipHeaders["content-length"])
+		if err != nil {
+			log.Println("Content-Length value cannot be converted to number, setting it to 0")
+			contentLength = 0
 		}
+		if contentLength < 0 {
+			log.Println("Content-Length value is less than 0, setting it to 0")
+			contentLength = 0
+		}
+		sipMessageBody := []byte{}
+		if contentLength != 0 {
+			sipMessageBody = make([]byte, contentLength, contentLength)
+			_, err := reader.Read(sipMessageBody)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+		inbound <- SipMessage{FirstLine: firstLine, Headers: sipHeaders, Body: string(sipMessageBody)}
 	}
 }
